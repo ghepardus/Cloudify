a116c52f39923fe29983e1a6ab0417f3146a997e
Mario Severino <mario.severino1986@gmail.com>
2022-08-22 08:51:13 +0200

    Initial Commit

---
+++ Cloudify/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cloudify
+//
+//  Created by Mario Severino on 22/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
